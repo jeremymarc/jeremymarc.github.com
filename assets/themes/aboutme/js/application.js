74e6e3c7e9a54aa9e197ed33e999ed1af3b838cd
Jeremy Marc <jeremy.marc@me.com>
2012-12-16 21:54:28 +0100

    update theme css/js

--- assets/themes/aboutme/js/application.js
+++ assets/themes/aboutme/js/application.js
@@ -1,14 +1,16 @@
-$(window).on('scroll', function(e) {
-    var isDark = false;
-    if ($(this).scrollTop() < 100) {
-        isDark = false;
-        $('#bg-image').stop().animate({
-            'opacity': '0.7',
-        }, 300);
-    } else if (!isDark) {
-        isDark = true;
-        $('#bg-image').stop().animate({
-            'opacity': '0.3',
-        }, 300);
-    }
+$(window).ready(function() {
+    $(window).on('scroll', function(e) {
+        var isDark = false;
+        if ($(this).scrollTop() < 100) {
+            isDark = false;
+            $('#bg-image').stop().animate({
+                'opacity': '0.7',
+            }, 300);
+        } else if (!isDark) {
+            isDark = true;
+            $('#bg-image').stop().animate({
+                'opacity': '0.3',
+            }, 300);
+        }
+    });
 });
